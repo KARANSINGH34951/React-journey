c31902b5600db154f80c065103879b3b8a98f395
karan singh D <ceitkaransingh2026@gmail.com>
2024-03-03 13:48:39 +0530

    vite added to react

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <h1>hello world <button>login</button> yoyo</h1>
+    
   );
 }
 
